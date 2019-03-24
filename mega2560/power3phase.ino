1bcfb0b316a7976024a36ed70281cf4db97a7ec5
yguan <yguan@example.com>
2019-03-24 17:47:15 +0800

    update

--- mega2560/power3phase.ino
+++ mega2560/power3phase.ino
@@ -3,13 +3,16 @@
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT);
-  
+
   // enable timer3's output.
   pinMode(5, OUTPUT); // OC3A
   pinMode(2, OUTPUT); // OC3B
   pinMode(3, OUTPUT); // OC3C
 
   noInterrupts();
+  // PRTIM3 must be set to 0 to enable timer/counter3 module.
+  PRR1 &= ~(1 << PRTIM3); 
+
   TCCR3A = 0; // clear first
   TCCR3B = 0;
 
@@ -18,7 +21,8 @@ void setup() {
   TCCR3A |= (1 << WGM31);
   TCCR3B |= (1 << WGM32);
   // clk/1024
-  TCCR3B |= (1 << CS32)|(1 << CS30);
+  //TCCR3B |= (1 << CS32)|(1 << CS30);
+  TCCR3B |= (1 << CS30);
 
   // non-inverting mode.  
   TCCR3A |= (1 << COM3A1); 
